8ffa154543fb85884aad0a66b107ccce0aca0c42
DEV Shinigami <cristiano.manuel.dias@gmail.com>
2025-01-05 23:40:06 +0000

    Update Pawly.js

--- Pawly.js
+++ Pawly.js
@@ -16,24 +16,3 @@ title.addEventListener("mouseout", () => {
     title.style.textShadow = "none";
 });
 
-// Seleciona o elemento "Pawly"
-const pawlyTrigger = document.getElementById("pawly-sound-trigger");
-const barkSound = document.getElementById("bark-sound-1");
-
-// Função para alternar o som
-function toggleBark() {
-    if (barkSound.paused) {
-        barkSound.currentTime = 0; // Reinicia o som
-        barkSound.play(); // Toca o som
-    } else {
-        barkSound.pause(); // Pausa o som
-        barkSound.currentTime = 0; // Reinicia para o início
-    }
-}
-
-// Adiciona o evento de clique
-pawlyTrigger.addEventListener("click", (event) => {
-    event.preventDefault(); // Previne a ação padrão do link
-    toggleBark(); // Chama a função para alternar o som
-});
-
